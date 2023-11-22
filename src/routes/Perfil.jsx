71e74d91b8f71f1e50bf1c926d1f4fe5bcb06017
Gabriel <gabmt2210@gmail.com>
2023-11-22 19:51:17 -0300

    perfil medico

--- src/routes/Perfil.jsx
+++ src/routes/Perfil.jsx
@@ -1,101 +1,166 @@
 import { useState, useEffect } from 'react';
-import "../styles/perfil.style.css";
+import '../styles/perfil.style.css';
 
 function Perfil() {
-  // Estado para armazenar as informações do usuário logado
   const [usuarioLogado, setUsuarioLogado] = useState(null);
   const [userIcon, setUserIcon] = useState(null);
+  const [mostrarVisitas, setMostrarVisitas] = useState(false);
+  const [mostrarResultados, setMostrarResultados] = useState(false);
+  const [pacienteInfo, setPacienteInfo] = useState(null);
 
-  // Efeito para carregar as informações do usuário do sessionStorage
   useEffect(() => {
-    try {
-      const userFromStorage = JSON.parse(sessionStorage.getItem('usuarioLogado'));
+    const fetchData = async () => {
+      try {
+        const userFromStorage = JSON.parse(sessionStorage.getItem('usuarioLogado'));
 
-      if (userFromStorage) {
-        setUsuarioLogado(userFromStorage);
-        setUserIcon(userFromStorage.foto);
+        if (userFromStorage) {
+          setUsuarioLogado(userFromStorage);
+          setUserIcon(userFromStorage.foto);
+
+          // Verifica se o usuário logado é um doutor e tem pacientes associados
+          if (userFromStorage.status === 'd' && userFromStorage.pacientes) {
+            const pacienteEmail = userFromStorage.pacientes[Object.keys(userFromStorage.pacientes)[0]];
+
+            const response = await fetch("http://localhost:3000/user");
+            if (response.ok) {
+              const users = await response.json();
+
+              // Corrigindo para verificar se pacienteEmail existe em users
+              if (users[pacienteEmail]) {
+                const paciente = users[pacienteEmail];
+                setPacienteInfo(paciente);
+              }
+            }
+          }
+        }
+      } catch (error) {
+        console.log('Erro ao carregar informações do paciente', error);
       }
-    } catch (error) {
-      console.log('no login');
-    }
+    };
+
+    fetchData();
   }, []);
 
-  // Renderizar informações com base no status do login
   return (
     <>
       <main className='bg-cinza'>
-      
         {usuarioLogado ? (
           <section className='profile'>
             <section className='perfilInfos bg-verde'>
-                
-            <img src={`/assets/${userIcon}`} alt="User Profile" className='ProfilePicture'/>
-            <h2 className='perfilNome texto-laranjaEscuro'>{usuarioLogado?.nome}</h2>
+              <img src={`/assets/${userIcon}`} alt="User Profile" className='ProfilePicture' />
+              <h2 className='perfilNome texto-laranjaEscuro'>{usuarioLogado?.nome}</h2>
             </section>
-            <h2 className='subtittle'>Informações de usuario</h2>
+            <h3 className='subtittle'>Informações de usuário</h3>
             <div className='personalInfos bg-branco'>
-            <h3>Email: {usuarioLogado.email}</h3>
-            <h3>Data de Nascimento: {usuarioLogado.dataNascimento}</h3>
-            <h3>Gênero: {usuarioLogado.genero}</h3>
-            <h3>Plano de saude: {usuarioLogado.plano}</h3>
+              <h3>Email: {usuarioLogado.email}</h3>
+              <h3>Data de Nascimento: {usuarioLogado.dataNascimento}</h3>
+              <h3>Gênero: {usuarioLogado.genero}</h3>
             </div>
-            
-            {usuarioLogado.status === "p" ? (
-              // Renderizar informações para pacientes
-              <div>
-                
-                {Object.keys(usuarioLogado.visitas).length > 0 ? (
-                  <>
-                  
-                    <h3>Visitas:</h3>
-                    <ul>
-                      {Object.entries(usuarioLogado.visitas).map(([data, visita]) => (
-                        <li key={data}>
-                          {`${data}: ${visita.local}, ${visita.descricao}`}
+
+            {usuarioLogado.status === 'p' ? (
+              <section>
+                <h3 className='subtittle'>Resultados</h3>
+                <div>
+                  <section className="box">
+                    <button onClick={() => setMostrarVisitas(!mostrarVisitas)} className='btn bg-branco'>
+                      {mostrarVisitas ? 'Ocultar Visitas' : 'Mostrar Visitas'}
+                    </button>
+                    <div className={mostrarVisitas ? 'visible' : 'hidden'} id='inside'>
+                      {mostrarVisitas && Object.keys(usuarioLogado.visitas).length > 0 ? (
+                        <div>
+                          <ul>
+                            {Object.entries(usuarioLogado.visitas).map(([data, visita]) => (
+                              <li key={data}>
+                                {`${data}: ${visita.local}, ${visita.descricao}`}
+                              </li>
+                            ))}
+                          </ul>
+                        </div>
+                      ) : null}
+                    </div>
+                  </section>
+
+                  <button onClick={() => setMostrarResultados(!mostrarResultados)} className='btn bg-branco'>
+                    {mostrarResultados ? 'Ocultar Resultados' : 'Mostrar Resultados'}
+                  </button>
+                  <div className={mostrarResultados ? 'visible' : 'hidden'} id='inside'>
+                    {mostrarResultados && Object.keys(usuarioLogado.resultados).length > 0 ? (
+                      <div>
+                        <ul>
+                          {Object.entries(usuarioLogado.resultados).map(([data, resultado]) => (
+                            <li key={data}>
+                              {`${data}: ${resultado.nome}, `}
+                              {resultado.link ? (
+                                <a href={`src/assets/pdfs/${resultado.link}`} target="_blank" rel="noopener noreferrer" className='pdf'>
+                                  Ver Resultado (PDF)
+                                </a>
+                              ) : (
+                                <span>Sem link disponível</span>
+                              )}
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+                    ) : null}
+                  </div>
+                </div>
+              </section>
+            ) : usuarioLogado.status === 'd' ? (
+              
+                <section>
+                  {pacienteInfo ? (
+                    <div>
+                      <h3 className='subtittle'>Informações do proximo Paciente</h3>
+                      <div className='historico' id="inside">
+                      <ul>
+                        <li>
+                          <h2>{pacienteInfo.nome}</h2>
+                          <p>Email: {pacienteInfo.email}</p>
+                          <p>Data de Nascimento: {pacienteInfo.dataNascimento}</p>
+                          <h2>Ultimas consultas</h2>
+                          <ul>
+                            {pacienteInfo.visitas && Object.keys(pacienteInfo.visitas).length > 0 ? (
+                              Object.entries(pacienteInfo.visitas).map(([data, visita]) => (
+                                <li key={data}>
+                                  {`${data}: ${visita.local}, ${visita.descricao}`}
+                                </li>
+                              ))
+                            ) : (
+                              <li>Sem histórico de visitas</li>
+                            )}
+                          </ul>
+                          <h2>Histórico de Resultados</h2>
+                          <ul>
+                            {pacienteInfo.resultados && Object.keys(pacienteInfo.resultados).length > 0 ? (
+                              Object.entries(pacienteInfo.resultados).map(([data, resultado]) => (
+                                <li key={data}>
+                                  {`${data}: ${resultado.nome}, `}
+                                  {resultado.link ? (
+                                    <a href={`/assets/pdfs/${resultado.link}`} target="_blank" rel="noopener noreferrer" className='pdf'>
+                                      Ver Resultado (PDF)
+                                    </a>
+                                  ) : (
+                                    <span>Sem link disponível</span>
+                                  )}
+                                </li>
+                              ))
+                            ) : (
+                              <li>Sem histórico de resultados</li>
+                            )}
+                          </ul>
                         </li>
-                      ))}
-                    </ul>
-                  </>
-                ) : (
-                  <p>Nenhuma visita registrada ainda.</p>
-                )}
-                {Object.keys(usuarioLogado.resultados).length > 0 ? (
-                  <>
-                    <h3>Resultados:</h3>
-                    <ul>
-                      {Object.entries(usuarioLogado.resultados).map(([data, resultado]) => (
-                        <li key={data}>
-                          {`${data}: ${resultado.nome}, `}
-                          {resultado.link ? (
-                            <a href={`src/assets/pdfs/${resultado.link}`} target="_blank" rel="noopener noreferrer">
-                              Ver Resultado (PDF)
-                            </a>
-                          ) : (
-                            <span>Sem link disponível</span>
-                          )}
-                        </li>
-                      ))}
-                    </ul>
-                  </>
-                ) : (
-                  <p>Nenhum resultado disponível ainda.</p>
-                )}
-              </div>
-            ) : usuarioLogado.status === "d" ? (
-              // Renderizar informações para médicos
-              <div>
-                <h3>Status: Doutor</h3>
-                {/* Renderizar outras informações específicas para doutores */}
-              </div>
+                      </ul>
+                      </div>
+                    </div>
+                  ) : null}
+                </section>
             ) : (
-              // Renderizar informações para outros casos
               <div>
                 <h3>Status: Desconhecido</h3>
               </div>
             )}
           </section>
         ) : (
-          // Caso não haja usuário logado, redirecione para a página de login
           <p>Usuário não autenticado. Redirecionando para a página de login...</p>
         )}
       </main>
